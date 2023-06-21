8534e9627ed180210a9bff4252de2582925f21f7
Tobias Aagaard <Tobias909@live.dk>
2023-06-21 09:36:16 +0200

    Test test

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path'
 
 
 const app = express();
 
-
+app.get("/", (req, res) => (
+    res.send('velkommen')
+))
 
 app.listen(4000, () => {
     console.log('The server is running on http://localhost:4000')
