4db03d2a0f056d91651203d68a31e909dd35d34b
Tobias Aagaard <Tobias909@live.dk>
2023-06-21 09:42:30 +0200

    Dotenv setup

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path'
 
+dotenv.config()
 
 const app = express();
+
+const PORT = process.env.PORT
 
 app.get("/", (req, res) => (
     res.send('velkommen')
 ))
 
 app.listen(4000, () => {
-    console.log('The server is running on http://localhost:4000')
+    console.log(`This server runs on http://localhost:${PORT} `)
 });
